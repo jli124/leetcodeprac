9307a540a169b16744fcd379c5e2cc9eca97bd32
Jiayue <lijiayue906@gmail.com>
2020-03-22 12:07:32 -0700

    bst2ndtm

--- BinarySearch/33searchrotatedarray.py
+++ BinarySearch/33searchrotatedarray.py
@@ -9,7 +9,7 @@ Binary search
 """
 
 #-------------------------------------------------------------------------------
-#    Soluton
+#    Soluton - logN
 #-------------------------------------------------------------------------------
 class Solution(object):
     def search(self, nums, target):
@@ -21,15 +21,21 @@ class Solution(object):
         left, right = 0, len(nums) -1
         while left <= right:
             mid = left + right >> 1
-            if nums[mid] == target:
+            # case 1 target found
+            if target == nums[mid]:
                 return mid
-            
-            if (target < nums[0] and not target < nums[mid] < nums[0] or
-                    target >= nums[0] and nums[0] <= nums[mid] < target):
-                left = mid +1
+            if target < nums[0]:
+                if target < nums[mid] < nums[0]:
+                    right = mid -1
+                else:
+                    left = mid +1
+            ## target >= nums[0]
             else:
-                right = mid -1
-        return -1        
+                if nums[0] <= nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid -1
+        return -1       
 
 #-------------------------------------------------------------------------------
 #    Test
